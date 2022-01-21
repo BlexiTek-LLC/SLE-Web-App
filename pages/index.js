312ba6dc60da4acdae250bd075534aead1d4bda1
kervdev21 <kervdev21@gmail.com>
2022-01-21 06:18:07 -0500

    Added a picture to the assets folder and also created a landingpage (the css will need some fixing so the layout looks right)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import LandingPage from '../components/Home/LandingPage'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <LandingPage />
       
     </div>
   )
