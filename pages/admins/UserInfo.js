82cb910b7848246eb9d46d02f9e86dc4f2fa428d
Stephenna J Corry <Sjcstephenna@gmail.com>
2022-02-08 09:51:53 -0500

    Created user info and login component

---
+++ pages/admins/UserInfo.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/Admin/UserInfo.module.css";
+import Image from "next/image";
+import Profile from "../../public/assets/male.jpg";
+
+const UserInfo = () => {
+  const [displaySelection, setDisplaySelection] = useState("I am a...");
+
+
+  const selection = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+    //  I need to refactor this
+    displaySelection ? setDisplaySelection('') : setDisplaySelection("I am a");
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.header}>Who are you?</h1>
+        <p className={styles.subheader}>Please enter your details</p>
+        <Image
+          // loader={myLoader}
+          src={Profile}
+          alt="Picture of the author"
+          width={200}
+          height={200}
+          className={styles.profileImage}
+        />
+        <form className={styles.form}>
+          <div className={styles.name}>
+              <label>Name:</label>
+            <input
+              type="text"
+              name="firstName"
+              id={styles.firstName}
+              placeholder="First name"
+            />
+            <input
+              type="text"
+              name="lastName"
+              id={styles.lastName}
+              placeholder="Last name"
+            />
+          </div>
+          <div className={styles.dob}>
+            <label>Date of Birth: </label>
+            <input type="date" name="birth" id={styles.birth} />
+          </div>
+            <button onClick={selection} className={styles.selection} style={{width: 500}}>{displaySelection}</button>
+            {/* Redoing this also */}
+            {!displaySelection && (
+              <select name="money" id="money">
+                <option value="loanee">Loanee</option>
+                <option value="Donor">Donor</option>
+              </select>
+            )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
