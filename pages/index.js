f8347f76788506ce051b1d574cb9d3e3218df49d
Kervin Philippe <kervin@Breeanas-MacBook-Pro.local>
2022-03-27 10:25:12 -0400

    Added conten for home page, added functional form for subscribers, and displayed the signature wall

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,24 @@ import LandingPage from '../components/Home/LandingPage'
 import SignatureWallPage from '../components/Home/SignatureWallPage'
 import LoadingScreen from '../components/Utils/LoadingScreen';
 import Navbar from '../components/Utils/Navbar';
+import { collection, addDoc, getDocs, getDoc } from "firebase/firestore"; 
+import {db} from '../components/Utils/Configs/firebase';
 
 import styles from '../styles/Home.module.css'
+import { async } from '@firebase/util';
 
 export default function Home() 
 {
   const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [userPost, setUserPost] = useState('');
+  const [uPost, setUpost] = useState([]);
 
   useEffect(
     () =>
     {
       setLoading(true)
+      setEmail('')
       setTimeout(
         () =>
         {
@@ -26,6 +33,46 @@ export default function Home()
       )
     }, []
   )
+
+  
+    const onSubmit = async(event) =>
+    {
+     event.preventDefault();
+      try 
+      {
+        const docRef = await addDoc(collection(db, 'Subscribe'),
+        {
+          email,
+          userPost
+        })
+        console.log("Document written with ID: ", docRef.id);
+        location.reload();  
+      } catch (error) 
+      {
+        console.error("Error adding document: ", e); 
+      }
+       
+    }
+
+    const getSubscribers =  () =>
+    {
+      const dbRef = collection(db, 'Subscribe');
+      getDocs(dbRef).then((data) =>
+      {
+        setUpost(data.docs.map((item) =>
+        {
+          return { ...item.data(), id: item.id}
+        }))
+      })
+    }
+
+    useEffect(() =>
+    {
+      getSubscribers();
+    }, [])
+
+
+
   return (
     <div className={styles.container}>
       {
@@ -40,6 +87,50 @@ export default function Home()
           </Head>
 
           <Navbar />
+          <p className={styles.title}>Black Owned, NFT Community Combatting Student Loan Debt</p>
+          <div className={styles.contentBox}>
+            <div className={styles.leftBox}>
+              <img src='/assets/landingPage/avatar.jpeg' className={styles.hAvatar}/>
+              <img src='/assets/landingPage/nfts.png' className={styles.hNft} />
+              <p className={styles.hSubtitle}>NFT MarketPlace Coming Soon...</p>
+            </div>
+            <div className={styles.rightBox}>
+              <p className={styles.cta}>Say something to the student loan industry...</p>
+              <div className={styles.signatureWall}>
+                <form className={styles.subscribe}  action='/'>
+                  <textarea
+                   className={styles.post}
+                   type='text'
+                   placeholder='Say it here...'
+                   value={userPost}
+                   onChange={()=> setUserPost(event.target.value)}
+                    />
+                  <input
+                   className={styles.email}
+                   type='email'
+                   placeholder='Email'
+                   value={email}
+                   onChange={() => setEmail(event.target.value)} />
+                  <button  onClick={onSubmit} className={styles.btn}>
+                    Submit
+                  </button>
+                </form>
+                <div className={styles.postBox}>
+                  {
+                    uPost.map((post) =>
+                    {
+                      return(
+                        <div className={styles.box}>
+                          <img src='/assets/landingPage/sleLogo.ico' className={styles.postImg}/>
+                          <p className={styles.userPosts}>{post.userPost}</p>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       }
     </div>
